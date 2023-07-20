16e5ce92168d731d88dc95a51de61fba561583c9
Jordan Needham <jdncreations@hotmail.com>
2023-07-20 11:11:13 +0800

    responsive mobile & desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss')",.Config} */
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(176, 50%, 47%)",
+        accent: "hsl(176, 72%, 28%)",
+        title: "hsl(0, 0%, 0%)",
+        text: "hsl(0, 0%, 48%)",
+      },
+    },
   },
   plugins: [],
 }
